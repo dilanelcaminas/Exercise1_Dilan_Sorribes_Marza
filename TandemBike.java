e6b6243b8fb45c9eb9454bab753823822ace0a19
dilanelcaminas <90651737+dilanelcaminas@users.noreply.github.com>
2021-10-25 12:42:40 +0200

    Add files via upload

---
+++ TandemBike.java
@@ -0,0 +1,9 @@
+class TandemBike extends Bicycle {
+    int numSeats = 2;
+    void applyBrakes(int decrement1, int decrement2) {
+        speed = speed - decrement1 - decrement2;
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+}
